65f50d8140acd9eeb64f33ef139b9d2340503d8c
Sifar <sifar@smile.fr>
2016-02-08 11:15:42 +0100

    Update home.php

--- app/controller/home.php
+++ app/controller/home.php
@@ -10,11 +10,38 @@
 class Home extends Controller
 {
     /**
+     * @var null userModel
+     */
+    public userModel = null;
+    
+    /**
+     * @var null privateMessageModel
+     */
+    public globalMessageModel = null;
+    /**
+     * Loads the "model".
+     * @return object model
+     */
+    public function loadModel()
+    {
+        require_once APP . 'model/userModel.php';
+        // TO DECOMMENT once the model is created
+        //require_once APP . 'model/globalMessageModel.php';
+        
+        // create new "model" (and pass the database connection)
+        $this->userModel = new UserModel($this->db);
+        // $this->globalMessageModel = new globalMessageModel($this->db);
+    }
+    
+    /**
     * PAGE: index
     * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
     */
     public function index()
     {
+        $currentUser = $this->userModel->getCurrentUser();
+		//$globalMessage = $this->globalMessageModel->getAllGlobalMessages();
+		
         // load views
         require APP . 'view/_templates/header.php';
         require APP . 'view/home/index.php';
