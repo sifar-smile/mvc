97830fb3afe0956eccd227748b30b38644def957
Sifar <sifar@smile.fr>
2016-02-08 12:14:19 +0100

    Update index.php

--- app/view/home/index.php
+++ app/view/home/index.php
@@ -2,3 +2,16 @@
     <h2>You are in the View: app/view/home/index.php (everything in the box comes from this file)</h2>
     <p>In a real application this could be the homepage.</p>
 </div>
+
+<h3>Globals messages</h3>
+<div>
+    <!-- lister les messages globaux ici -->
+</div>
+<?php if ($currentUser) : ?>
+    <!-- formulaire d'ajout d'un global message -->
+	<form action="<?php echo URL; ?>home/addGlobalMessage" method="POST">
+	            <label>Message</label>
+	            <input type="text" name="texte" value="" required />
+	            <input type="submit" name="submit_add_gm" value="Submit" />
+	</form>
+<?php endif; ?>	
