8190a87e228eab8a3f2d78d4e310851d9e35c2fc
Sifar <sifar@smile.fr>
2016-02-10 11:35:51 +0100

    Update datas.sql

--- ressources/datas.sql
+++ ressources/datas.sql
@@ -1,104 +1,142 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- MySQL dump 10.13  Distrib 5.1.73, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: mydb
+-- ------------------------------------------------------
+-- Server version	5.1.73-1
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+--
+-- Table structure for table `global_message`
+--
 
--- -----------------------------------------------------
--- Table `mydb`.`user`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`user` ;
-
-CREATE TABLE IF NOT EXISTS `mydb`.`user` (
-   id SERIAL ,
-  `login` VARCHAR(45) NULL DEFAULT NULL,
-  `password` VARCHAR(45) NULL DEFAULT NULL,
-  `lastname` VARCHAR(45) NULL DEFAULT NULL,
-  `firstname` VARCHAR(45) NULL DEFAULT NULL,
-  `birthday` TIMESTAMP NULL DEFAULT NULL,
-  `status` VARCHAR(100) NULL DEFAULT NULL,
-  `avatar` VARCHAR(200) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`));
-
-
--- -----------------------------------------------------
--- Table `mydb`.`post`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`post` ;
-
-CREATE TABLE IF NOT EXISTS `mydb`.`post` (
-   id SERIAL ,
-  `texte` VARCHAR(2000) NOT NULL,
-  `date` TIMESTAMP NOT NULL,
-  `image` VARCHAR(200) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`));
-
-
--- -----------------------------------------------------
--- Table `mydb`.`private_message`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`private_message` ;
-
-CREATE TABLE IF NOT EXISTS `mydb`.`private_message` (
-   id SERIAL ,
-  `sender` INT NULL DEFAULT NULL,
-  `recipient` INT NULL DEFAULT NULL,
-  `parent` INT NULL DEFAULT NULL,
-  `post` INT NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `global_message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `global_message` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `post` int(11) DEFAULT NULL,
+  `sender` int(11) DEFAULT NULL,
+  `like` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_message_utilisateur1` (`sender` ASC),
-  INDEX `fk_message_utilisateur2` (`recipient` ASC),
-  INDEX `fk_message_post1` (`post` ASC),
-  CONSTRAINT `fk_message_utilisateur1`
-    FOREIGN KEY (`sender`)
-    REFERENCES `mydb`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_message_utilisateur2`
-    FOREIGN KEY (`recipient`)
-    REFERENCES `mydb`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_message_post1`
-    FOREIGN KEY (`post`)
-    REFERENCES `mydb`.`post` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  UNIQUE KEY `id` (`id`),
+  KEY `fk_chat_post1` (`post`),
+  KEY `fk_chat_utilisateur1` (`sender`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `global_message`
+--
 
--- -----------------------------------------------------
--- Table `mydb`.`global_message`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`global_message` ;
+LOCK TABLES `global_message` WRITE;
+/*!40000 ALTER TABLE `global_message` DISABLE KEYS */;
+/*!40000 ALTER TABLE `global_message` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `mydb`.`global_message` (
-   id SERIAL ,
-  `post` INT NULL DEFAULT NULL,
-  `sender` INT NULL DEFAULT NULL,
-  `like` INT NULL DEFAULT NULL,
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `texte` varchar(2000) NOT NULL,
+  `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `image` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_chat_post1` (`post` ASC),
-  INDEX `fk_chat_utilisateur1` (`sender` ASC),
-  CONSTRAINT `fk_chat_post1`
-    FOREIGN KEY (`post`)
-    REFERENCES `mydb`.`post` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_chat_utilisateur1`
-    FOREIGN KEY (`sender`)
-    REFERENCES `mydb`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `post`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `private_message`
+--
+
+DROP TABLE IF EXISTS `private_message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `private_message` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `sender` int(11) DEFAULT NULL,
+  `recipient` int(11) DEFAULT NULL,
+  `parent` int(11) DEFAULT NULL,
+  `post` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `fk_message_utilisateur1` (`sender`),
+  KEY `fk_message_utilisateur2` (`recipient`),
+  KEY `fk_message_post1` (`post`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `private_message`
+--
+
+LOCK TABLES `private_message` WRITE;
+/*!40000 ALTER TABLE `private_message` DISABLE KEYS */;
+/*!40000 ALTER TABLE `private_message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `birthday` timestamp NULL DEFAULT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  `avatar` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'greiiii','430502bf70edcfa4f07892536cabbcb1e2f6471f','gre','gre',NULL,NULL,NULL),(2,'greiiii','430502bf70edcfa4f07892536cabbcb1e2f6471f','gre','gre',NULL,NULL,NULL),(3,'grei','430502bf70edcfa4f07892536cabbcb1e2f6471f','gre','gre',NULL,NULL,NULL),(4,'grei','430502bf70edcfa4f07892536cabbcb1e2f6471f','gre','gre',NULL,NULL,NULL),(5,'grei','430502bf70edcfa4f07892536cabbcb1e2f6471f','gre','gre',NULL,NULL,NULL),(6,'grei','430502bf70edcfa4f07892536cabbcb1e2f6471f','gre','gre',NULL,NULL,NULL),(7,'gerger','ed22c22dbde360207569092e60b4298397efb8da','gergerg','erger',NULL,NULL,NULL),(8,'hrthtr','8b27b3d75fd6f156899429740b35191f31f109c2','hrth','htrh',NULL,NULL,NULL),(9,'hrthtr','8b27b3d75fd6f156899429740b35191f31f109c2','hrth','htrh',NULL,NULL,NULL),(10,'gre','79e2475f81a6317276bf7cbb3958b20d289b78df','erger','erg',NULL,NULL,NULL),(11,'gerg','ed22c22dbde360207569092e60b4298397efb8da','gergerg','erger',NULL,NULL,NULL),(12,'htrh','fa6af6e97d010a98b5bfb9dc17862112afda402e','hrth','rthrt',NULL,NULL,NULL),(13,'jhjrt','825d40aa2f3d9131ad45f5d26966405015c29ab7','rtjrtj','jrtj',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-01-27  8:55:00
